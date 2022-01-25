41a089a9d36da55cfc48a2de69ac0b1de47ddf08
Andrew-Ogundimu <56798377+Andrew-Ogundimu@users.noreply.github.com>
2022-01-25 08:02:22 -0500

    Add files via upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.robotcore.hardware.ServoController;
+
 /**
  * This class is for controlling the robot with the gamepad controller.
  **/
@@ -9,6 +11,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -25,14 +28,40 @@ import java.util.Map;
 @TeleOp(name="Tele-Op V1", group="Iterative Opmode")
 public class Teleop extends OpMode
 {
+    public class Arm {
+        /*
+        Initializes the Arm segment lengths in cm
+         */
+        private float segment1;
+        private float segment2;
+        public Arm(float length1,float length2) {
+            segment1 = length1;
+            segment2 = length2;
+        }
+        public float[] CalcServos(float dx, float dy) {
+            float targ1 = (float)(Math.acos(
+                    (segment2*segment2-segment2*segment2-dx*dx-dy*dy)/
+                            (-2*segment1*Math.sqrt(dx*dx+dy*dy)))+Math.atan(dy/dx));
+            float targ2 = (float)Math.acos(
+                    (dx*dx+dy*dy-segment1*segment1-segment2*segment2)/
+                            (-2*segment1*segment2));
+            return new float[]{targ1/(float)Math.PI,targ2/(float)Math.PI};
+        }
+    }
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor m0 = null;
     private DcMotor m1 = null;
     private DcMotor m2 = null;
     private DcMotor m3 = null;
-    private Servo arm1 = null;
+    private Servo arm1_1 = null;
+    private Servo arm1_2 = null;
     private Servo arm2 = null;
+    private CRServo wheel = null;
+    private Servo grab = null;
+    private ServoController sc;
+    private Arm total = new Arm(250f,250f);
+    private float[] targ_pos = new float[]{105f,60f};
     private double hFOV = 60; //Horizontal FOV in degrees
     private double vFOV; //vertical FOV in degrees
     private double focal_length; //focal length in pixels
@@ -62,7 +91,7 @@ public class Teleop extends OpMode
         object_sizes.put("Ball",69.9);
         object_sizes.put("Cube",50.8);
         object_sizes.put("Duck",51.86); //average of the width,length, and height
-        telemetry.addData("Status", "Initialized");
+
 
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
@@ -71,7 +100,12 @@ public class Teleop extends OpMode
         m1 = hardwareMap.get(DcMotor.class, "m1"); // fr
         m2 = hardwareMap.get(DcMotor.class, "m2"); // bl
         m3 = hardwareMap.get(DcMotor.class, "m3"); // br
-
+        arm1_1 = hardwareMap.get(Servo.class, "bottom_arm1");
+        arm1_2 = hardwareMap.get(Servo.class,"bottom_arm2");
+        arm2 = hardwareMap.get(Servo.class,"top_arm1");
+        grab = hardwareMap.get(Servo.class,"grab");
+        wheel = hardwareMap.get(CRServo.class,"wheel");
+        sc = arm1_1.getController();
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
         m0.setDirection(DcMotor.Direction.FORWARD);
@@ -93,11 +127,12 @@ public class Teleop extends OpMode
             // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
             // should be set to the value of the images used to create the TensorFlow Object Detection model
             // (typically 16/9).
-            //tfod.setZoom(2.5, 16.0/9.0);
+            tfod.setZoom(2.5, 16.0/9.0);
         }
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
+        arm1_2.setDirection(Servo.Direction.REVERSE);
     }
 
     /*
@@ -105,6 +140,9 @@ public class Teleop extends OpMode
      */
     @Override
     public void init_loop() {
+        telemetry.addData("Servo Controller",sc.getServoPosition(arm1_1.getPortNumber()));
+        telemetry.addData("Servo Controller",sc.getServoPosition(arm1_2.getPortNumber()));
+        //sc.setServoPosition(arm1_1.getPortNumber(),0.25);
     }
 
     /*
@@ -127,17 +165,43 @@ public class Teleop extends OpMode
      */
     @Override
     public void loop() {
-        double m0_power = gamepad1.left_stick_x; // left or right
-        double m2_power = gamepad1.left_stick_x; // left or right
+        //arm1_1.setPower(1.0f);
+        //arm1_2.setPower(1.0f);
+        if (gamepad1.a) {
+            grab.setPosition(1.0f);
+        } else {
+            grab.setPosition(0.0f);
+        }
+
+        double m0_power = -gamepad1.left_stick_x; // left or right
+        double m2_power = -gamepad1.left_stick_x; // left or right
 
         double m3_power = gamepad1.left_stick_y; // up or down
         double m1_power = gamepad1.left_stick_y; // up or down
+
+        targ_pos[0]+=(gamepad1.dpad_left ? 1:0)-(gamepad1.dpad_right ? 1:0);
+        targ_pos[1]+=(gamepad1.dpad_up ? 1:0)-(gamepad1.dpad_down ? 1:0);
+        telemetry.addData("Target (mm)",Float.toString(targ_pos[0])+","+Float.toString(targ_pos[1]));
+        if (gamepad1.right_stick_x!=0) {
+            m0_power = gamepad1.right_stick_x;
+            m1_power = gamepad1.right_stick_x;
+            m2_power = -gamepad1.right_stick_x;
+            m3_power = -gamepad1.right_stick_x;
+        }
 
         m0.setPower(m0_power);
         m2.setPower(m2_power);
 
         m3.setPower(m3_power);
         m1.setPower(m1_power);
+
+        telemetry.addData("Test","");
+
+        float[] angles = total.CalcServos(targ_pos[0],targ_pos[1]);
+        arm1_1.setPosition((double)angles[0]);
+        arm1_2.setPosition((double)angles[0]);
+        arm2.setPosition((double)angles[1]);
+        telemetry.addData("Angles",Float.toString(angles[0]*180.0f)+" "+Float.toString(angles[1]*180.0f));
 
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
